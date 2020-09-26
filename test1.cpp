d7d0e03260691bed7cb5c0fcc4ebebe0e31d726c
labPC <freedom19970821@gmail.com>
2020-09-26 17:08:56 +0800

    a little change for test what will happen when compile error occurs

--- test1.cpp
+++ test1.cpp
@@ -4,7 +4,7 @@ int main(){
     std::cout << "This is a git test file!" << std::endl;
 
     NewCompileUnit unitTest(5);
-    std::cout << "The Unit value is " << unitTest.getValue();
-    
+    std::cout << "The Unit value is " << unitTest;
+
     return 0;
 }
